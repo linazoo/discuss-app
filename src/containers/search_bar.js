2e7127799bd510f9a446ad78f7643d13177cf08e
Lina Zoo <linazooper@gmail.com>
2017-11-29 23:08:38 -0600

    binding and replacing input change with a new instance for the search bar functionality

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    //constructor will run whenever this is instantiated (Searchbar instance) it runs this function
+    //constructor function is always ran when an instance is instantiated 
+    //setting it to an empty string in the beginning here:
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' }; 
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+        this.setState({ term: event.target.value });
+    }
+
+
     render() {
         return (
             <form className="input-group">
-                <input />
+                <input 
+                    placeholder="Get a five-day forecast in your favorite cities"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange} />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Search</button>
-                    </span>
+                </span>
             </form>
         );
     }
