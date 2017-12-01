4f3a868641dcfbbbfd0ab2f7cd08cd0953e8565c
Lina Zoo <linazooper@gmail.com>
2017-11-30 21:00:43 -0600

    ajax requests being made through axios to get weather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,12 @@
 const API_KEY = "affbfd14c28742e0f521908f21ab5ea0";
+const URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+
+    return {
+        type: FETCH_WEATHER
+    };
+}
