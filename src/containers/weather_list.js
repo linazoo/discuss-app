7095ba3798be6abebc478a57195b464c5f7af046
Lina Zoo <linazooper@gmail.com>
2017-12-06 23:18:08 -0600

    testing the waters of the search bar

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -4,40 +4,22 @@ import Chart from '../components/chart';
 import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
-    renderWeather(cityData) {
-        const name = cityData.city.name;
-        const temps = cityData.list.map(weather => weather.main.temp);
-        const pressures = cityData.list.map(weather => weather.main.pressure);
-        const humidities = cityData.list.map(weather => weather.main.humidity);
-        const { lon, lat } = cityData.city.coord;
-        
-        
-        
-        return (
-            <tr key={ name } >
-                <td><GoogleMap lon={ lon } lat={ lat } /></td>
-                <td><Chart data={ temps } color="orange" units="K"/></td>
-                <td><Chart data={ pressures } color="pink" units="hPa"/></td>
-                <td><Chart data={ humidities } color="red" units="%"/></td>                
-            </tr>
-        );
+    renderWeather(viewData) {
+        if (!viewData.length) {
+            return
+        };
+        const posts = viewData.data.children.map((view) => {
+            return (
+                <h2> { view.data.title }</h2>
+            )
+        });
     }
 
     render() {
         return (
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th>City</th>
-                        <th>Temperature (K)</th>
-                        <th>Pressure (hPa)</th>
-                        <th>Humidity (%) </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    { this.props.weather.map(this.renderWeather) }
-                </tbody>
-            </table>
+            <div>
+               { this.renderWeather(this.props.weather)}
+            </div>
         );
     }
 }
